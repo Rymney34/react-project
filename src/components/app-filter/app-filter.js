2d83fdc4d1494c69b162408ed77ca7dedb6b86f8
Rymney34 <locktimil70@gmail.com>
2025-06-17 18:31:23 +0100

    React first full application without interactive features

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,23 @@
+import './app-filter.css';
+
+const AppFilter = () => {
+    return(
+        <div className="btn-group">
+            <button className="btn btn-light" type="button">
+                All employees
+            </button>
+            <button className="btn btn-outline-light" type="button">
+                Salary to be incresed 
+            </button>
+            <button className="btn btn-outline-light" type="button">
+                Income over £1000
+            </button>
+            <button className="btn btn-light" type="button">
+                All employees
+            </button>
+
+        </div>
+    )
+}
+
+export default AppFilter
---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees count in the company</h1>
+            <h2> Amount of Employees: </h2>
+            <h2> Monthly Bonus will get: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
